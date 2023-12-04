b1cfc199b877001835130b0feefb804d8b1feff4
Luis Alho <lmalho@gmail.com>
2023-12-04 12:52:26 +0000

    day4

---
+++ advent-of-code-2023/day4/code.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"advent-of-code/utils"
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+)
+
+func processLine(counter int, line string, cardMap map[int]int) (int, int, map[int]int) {
+	counter++
+	//Get Game id
+
+	gameIdIndex := strings.Index(line, ":")
+	numbers := strings.Split(line[gameIdIndex+1:], "|")
+
+	winningNumbers := utils.ConvertStringSliceToIntSlice(strings.Split(strings.TrimSpace(numbers[0]), " "))
+	cardNumbers := utils.ConvertStringSliceToIntSlice(strings.Split(strings.TrimSpace(numbers[1]), " "))
+
+	slices.Sort(winningNumbers)
+	slices.Sort(cardNumbers)
+
+	countWinningNumbers := 0
+
+	cardMap[counter] += 1
+
+	for _, number := range cardNumbers {
+		_, found := slices.BinarySearch(winningNumbers, number)
+		if found {
+			countWinningNumbers++
+		}
+	}
+	for i := 1; i <= countWinningNumbers; i++ {
+		cardMap[counter+i] += cardMap[counter]
+	}
+
+	return counter, int(math.Pow(2, float64(countWinningNumbers-1))), cardMap
+}
+
+func main() {
+	// read input.txt file
+	// for each line, process the line
+	// print the result
+
+	p := filepath.Join("input", "input.txt")
+
+	file, err := os.Open(p)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	var results []int
+	cardMap := make(map[int]int)
+	scanner := bufio.NewScanner(file)
+	points := 0
+	counter := 0
+
+	for scanner.Scan() {
+		counter, points, cardMap = processLine(counter, scanner.Text(), cardMap)
+
+		results = append(results, points)
+	}
+
+	fmt.Println("Result part one: ", utils.SumSliceValues(results))
+	fmt.Println("Result part two: ", utils.SumMapValues(cardMap))
+
+}
--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,7 @@
 package utils
 
+import "strconv"
+
 func SumSliceValues(slice []int) int {
 	var sum int
 
@@ -17,4 +19,27 @@ func MultiplyMapValues(validMap map[string]int) int {
 		mult *= v
 	}
 	return mult
+}
+
+func ConvertStringSliceToIntSlice(stringSlice []string) []int {
+	var intSlice []int
+
+	for _, v := range stringSlice {
+		if v == "" {
+			continue
+		}
+		intElement, _ := strconv.Atoi(v)
+		intSlice = append(intSlice, intElement)
+	}
+
+	return intSlice
+}
+
+func SumMapValues(validMap map[int]int) int {
+	var sum int
+
+	for _, v := range validMap {
+		sum += v
+	}
+	return sum
 }
